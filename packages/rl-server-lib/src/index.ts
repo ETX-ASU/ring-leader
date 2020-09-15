ed61969313886ee858a08a3aed0626065f46e310
Devesh Tiwari <dtiwari@ats360.com>
2020-09-15 11:43:15 +0530

    Setting up folder structure and initial files

--- packages/rl-server-lib/src/index.ts
+++ packages/rl-server-lib/src/index.ts
@@ -1,1 +1,3 @@
-export {};
+import { ltiInitiateOIDC } from "./authentication/ltiInitiateOIDC";
+import { LtiHandleOAuth2Response } from "./authentication/ltiProcessOAuthFlow";
+export { ltiInitiateOIDC, LtiHandleOAuth2Response };
