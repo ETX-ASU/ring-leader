708cd5faa521b6eb21176f2fc5cabfa49b82ec3f
Devesh Tiwari <dtiwari@ats360.com>
2020-09-21 17:29:38 +0530

    bug fix

--- packages/rl-client-lib/src/index.ts
+++ packages/rl-client-lib/src/index.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line node/no-extraneous-import
 import { NamesAndRoles } from "@asu-etx/rl-server-lib";
+
 const getUsers = (token: string): any => {
   return new NamesAndRoles().getMembers(token, {});
 };
