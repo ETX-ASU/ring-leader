0e621b96721f343c49c940a2380731b58d3d8eb7
Devesh Tiwari <dtiwari@ats360.com>
2020-09-24 13:38:21 +0530

    bug fix

--- packages/rl-client-lib/src/index.ts
+++ packages/rl-client-lib/src/index.ts
@@ -1,5 +1,5 @@
 import { NamesAndRoles, RlPlatform } from "@asu-etx/rl-server-lib";
-const getUsers = (platform: any): any => {
-  return new NamesAndRoles().getMembers(platform);
+const getUsers = (platform: any, options?: any): any => {
+  return new NamesAndRoles().getMembers(platform, options);
 };
 export { getUsers, RlPlatform };
--- packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
+++ packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
@@ -15,7 +15,7 @@ const ltiServiceEndpoints = (app: Express): void => {
     const platform: any = req.session.platform;
     console.log("req.session.platform - " + JSON.stringify(platform));
     // pass the token from the session to the rl-client-lib to make the call to Canvas
-    const results = await getUsers(platform);
+    const results = await getUsers(platform, { role: "Learner" });
     res.send(results);
   });
 
