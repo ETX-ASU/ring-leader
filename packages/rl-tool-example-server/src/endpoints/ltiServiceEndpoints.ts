cdf03ff508735e6d116f94ff81d705b5eee3acc8
James Stanley <jstanley@unicon.net>
2020-09-29 13:22:44 -0700

    more messing around

--- packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
+++ packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
@@ -42,14 +42,14 @@ const ltiServiceEndpoints = (app: Express): void => {
     const newLineItemData = {
       scoreMaximum: lineItemData.scoreMaximum,
       label: lineItemData.label,
-      resourceId: "1",
-      resourceLinkId: "f586b531937ec6de8c616546c606fe87eb278ae7",
-      tag: lineItemData.tag,
-      "https://canvas.instructure.com/lti/submission_type": {
-        type: "external_tool",
-        external_tool_url: "https://ring-leader-james-stanley.herokuapp.com/lti-advantage-launch"
-        //lineItemData.label
-      }
+      //resourceId: "1",
+      //resourceLinkId: "f586b531937ec6de8c616546c606fe87eb278ae7",
+      tag: lineItemData.tag
+      //"https://canvas.instructure.com/lti/submission_type": {
+      //type: "external_tool",
+      //external_tool_url: "https://ring-leader-james-stanley.herokuapp.com/lti-advantage-launch"
+      //lineItemData.label
+      //}
     };
     const results = await createLineItem(platform, newLineItemData);
 
