03b03bbaa4777daecf9314b08f66b0a07b6c5a89
Devesh Tiwari <dtiwari@ats360.com>
2020-09-29 18:46:54 +0530

    bug fix

--- packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
+++ packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
@@ -82,12 +82,7 @@ const ltiServiceEndpoints = (app: Express): void => {
         comment: "This is exceptional work.",
         activityProgress: "Completed",
         gradingProgress: "FullyGraded",
-        userId: "7cae08ba-5ecc-457a-835e-4b9b7bff806c",
-        "https://canvas.instructure.com/lti/submission": {
-          new_submission: true,
-          submission_type: "online_url",
-          submission_data: "https://instructure.com"
-        }
+        userId: "7cae08ba-5ecc-457a-835e-4b9b7bff806c"
       },
       {
         id: "https://unicon.instructure.com/api/lti/courses/718/line_items/188"
