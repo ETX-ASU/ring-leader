3c865c9011add98af374576019115458983b1b04
Devesh Tiwari <dtiwari@ats360.com>
2020-09-30 16:27:47 +0530

    bug fix for creating line item

--- packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
+++ packages/rl-tool-example-server/src/endpoints/ltiServiceEndpoints.ts
@@ -44,7 +44,6 @@ const ltiServiceEndpoints = (app: Express): void => {
       scoreMaximum: lineItemData.scoreMaximum,
       label: lineItemData.label,
       resourceId: resourceId,
-      resourceLinkId: platform.resourceLinkId,
       tag: lineItemData.tag,
       "https://canvas.instructure.com/lti/submission_type": {
         type: "external_tool",
