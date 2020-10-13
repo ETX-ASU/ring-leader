e56ff3f40b28858de312fc63f6cbbf77122e5953
Devesh Tiwari <dtiwari@ats360.com>
2020-10-13 16:49:06 +0530

    bug fix

--- packages/rl-tool-example-client/src/routes/RouteInstructor/RouteInstructor.tsx
+++ packages/rl-tool-example-client/src/routes/RouteInstructor/RouteInstructor.tsx
@@ -246,15 +246,13 @@ const RouteInstructor: React.FC = () => {
               </div>
 
               <div className="form-group">
-                <div className="col-sm-offset-2 col-sm-10">
-                  <button
-                    type="submit"
-                    onClick={createAssignment}
-                    className="btn btn-primary"
-                  >
-                    Submit
-                  </button>
-                </div>
+                <button
+                  type="submit"
+                  onClick={createAssignment}
+                  className="btn btn-primary"
+                >
+                  Save
+                </button>
               </div>
             </div>
           )}
