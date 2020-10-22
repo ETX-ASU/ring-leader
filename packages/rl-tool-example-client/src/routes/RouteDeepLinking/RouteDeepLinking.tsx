96627a308eec6787d357053bf64a4f260e0e43e1
James Stanley <jstanley@unicon.net>
2020-10-22 13:18:54 -0700

    removed asu package

--- packages/rl-tool-example-client/src/routes/RouteDeepLinking/RouteDeepLinking.tsx
+++ packages/rl-tool-example-client/src/routes/RouteDeepLinking/RouteDeepLinking.tsx
@@ -2,9 +2,11 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./RouteDeepLinking.scss";
-import {DEEP_LINK_RESOURCELINKS_ENDPOINT, DEEP_LINK_ASSIGNMENT_ENDPOINT} from "@asu-etx/rl-client-lib";
+//import {DEEP_LINK_RESOURCELINKS_ENDPOINT, DEEP_LINK_ASSIGNMENT_ENDPOINT} from "@asu-etx/rl-client-lib";
 import $ from "jquery";
 
+const DEEP_LINK_RESOURCELINKS_ENDPOINT = "/lti-service/getDeepLinkAssignments";
+const DEEP_LINK_ASSIGNMENT_ENDPOINT = "/lti-service/deeplink";
 const RouteDeepLinking: React.FC = () => {
   const [resourceLink, setResourceLink] = useState<{}>({});
   const [assignments, setAssignments] = useState<any[]>([]);
