66c22cb8178cce43cc6f2f8e6999bab0199ac39f
James Stanley <jstanley@unicon.net>
2020-11-04 14:25:09 -0700

    still working on bugs

--- packages/rl-shared/src/util/environment.ts
+++ packages/rl-shared/src/util/environment.ts
@@ -1,9 +1,9 @@
 // heroku
 const ENV_VARS = process.env;
-console.log(`process.env in environments.ts: ${JSON.stringify(process.env)}`);
+console.log(`ENV_VARS in environments.ts: ${JSON.stringify(ENV_VARS)}`);
 console.log(`ENV_VARS.INSTRUCTOR_REDIRECT: ${ENV_VARS.INSTRUCTOR_REDIRECT}`);
-console.log(`ENV_VARS.[INSTRUCTOR_REDIRECT]: ${ENV_VARS["INSTRUCTOR_REDIRECT"]}`);
-
+console.log(`ENV_VARS[INSTRUCTOR_REDIRECT]: ${ENV_VARS["INSTRUCTOR_REDIRECT"]}`);
+console.log(`ENV_VARS.platform: ${ENV_VARS.platform}`);
 export const PORT: number = parseInt(ENV_VARS.PORT ? ENV_VARS.PORT : "8080");
 
 // this is set by the yarn run heroku-update-configs script
