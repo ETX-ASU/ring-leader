7dc5b19872d9de100232eecab98b124df69de16d
James Stanley <jstanley@unicon.net>
2020-11-08 22:19:04 -0700

    moe fun with params

--- packages/rl-client-lib/src/services/ValidateSessionService.ts
+++ packages/rl-client-lib/src/services/ValidateSessionService.ts
@@ -2,15 +2,14 @@ import axios from "axios";
 // eslint-disable-next-line node/no-extraneous-import
 import { useLocation } from "react-router-dom";
 import {API_URL, LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
-
+import queryString from 'query-string';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
 
 const hasValidSession = async (): Promise<boolean> => {
   logger.debug(`hitting endpoint GET:${LTI_SESSION_VALIDATION_ENDPOINT}`);
-  const params = useQuery();
+  const location = useLocation();
+  console.log(queryString.parse(location.search));
+  const params = queryString.parse(location.search);
   const hasValidSession = await axios
     .get(
       API_URL +
