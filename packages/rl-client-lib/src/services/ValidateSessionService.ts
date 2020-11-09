9db301e62f56ad34daa3991476599e266d6b47a9
James Stanley <jstanley@unicon.net>
2020-11-08 22:47:01 -0700

    adding query-string

--- packages/rl-client-lib/src/services/ValidateSessionService.ts
+++ packages/rl-client-lib/src/services/ValidateSessionService.ts
@@ -1,28 +1,28 @@
 import axios from "axios";
 // eslint-disable-next-line node/no-extraneous-import
 import { useLocation } from "react-router-dom";
-import {API_URL, LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
-import queryString from 'query-string';
+import { API_URL, LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
+import queryString = require("query-string");
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 
 const hasValidSession = async (): Promise<boolean> => {
   logger.debug(`hitting endpoint GET:${LTI_SESSION_VALIDATION_ENDPOINT}`);
-  
-  
+
+
   const location = useLocation();
-  console.log(queryString.parse(location.search));
-  const params = queryString.parse(location.search);
-  const hasValidSession = await axios
-    .get(
-      API_URL +
-        LTI_SESSION_VALIDATION_ENDPOINT +
-        `?platform=${params.get("platform")}`
-    )
-    .then((results) => {
-      logger.debug(JSON.stringify(results));
-      return results.data;
-    });
-  return hasValidSession.isValid;
+  console.log(location.search);
+  const parsed = queryString.parse(location.search);
+  const platform = parsed.platform;
+  if (platform) {
+    const hasValidSession = await axios
+      .get(API_URL + LTI_SESSION_VALIDATION_ENDPOINT + `?platform=${platform}`)
+      .then((results) => {
+        logger.debug(JSON.stringify(results));
+        return results.data;
+      });
+    return hasValidSession.isValid;
+  }
+  return false;
 };
 
 export { hasValidSession };
