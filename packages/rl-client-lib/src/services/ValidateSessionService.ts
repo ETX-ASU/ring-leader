738974e35310ea44c325e1b24f6f0d51cb37cad2
James Stanley <jstanley@unicon.net>
2020-11-08 21:51:45 -0700

    fixed hook

--- packages/rl-client-lib/src/services/ValidateSessionService.ts
+++ packages/rl-client-lib/src/services/ValidateSessionService.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
-import {useParams, useLocation} from "react-router-dom";
+// eslint-disable-next-line node/no-extraneous-import
+import { useLocation } from "react-router-dom";
 import {API_URL, LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
 
-const params = new URLSearchParams(useLocation().search);
-console.log(params.get("platform"));
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const hasValidSession = async (): Promise<boolean> => {
   logger.debug(`hitting endpoint GET:${LTI_SESSION_VALIDATION_ENDPOINT}`);
+  const params = useQuery();
   const hasValidSession = await axios
     .get(
       API_URL +
