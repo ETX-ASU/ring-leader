df5354f2abc1f526fdc3d9bbe09a8a9d38c1f0d0
James Stanley <jstanley@unicon.net>
2020-11-09 20:30:40 -0700

    this updates headers to allow a wider origin

--- packages/rl-server-lib/src/endpoints/cacheLtiServiceExpressEndpoints.ts
+++ packages/rl-server-lib/src/endpoints/cacheLtiServiceExpressEndpoints.ts
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line node/no-extraneous-import
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 import { getToolConsumerByName } from "../services/ToolConsumerService";
 import requestLogger from "../middleware/requestLogger";
 import {
@@ -52,73 +52,79 @@ function getSession(req: any): any {
     return session;
 }
 
+function send(response: Response) {
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT");
+    return response;
+}
 const cacheLtiServiceExpressEndpoints = (app: Express): void => {
-    app.get(ROSTER_ENDPOINT, requestLogger, async (req: any, res: any) => {
-
-        res.send(await getRoster(getPlatform(req), req.query.role));
+    app.get(ROSTER_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
+        
+        send(res).send(await getRoster(getPlatform(req), req.query.role));
     });
 
-    app.get(GET_UNASSIGNED_STUDENTS_ENDPOINT, requestLogger, async (req: any, res: any) => {
+    app.get(GET_UNASSIGNED_STUDENTS_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
 
         const reqQueryString: any = req.query;
         if (reqQueryString && reqQueryString.lineItemId) {
-            res.send(
+            send(res).send(
                 await getUnassignedStudents(getPlatform(req), req.query.resourceLinkId)
             );
         }
     });
 
-    app.get(GET_ASSIGNED_STUDENTS_ENDPOINT, requestLogger, async (req: any, res: any) => {
+    app.get(GET_ASSIGNED_STUDENTS_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
         const lineItemId: any = req.query.lineItemId;
         const resourceLinkId: any = req.query.resourceLinkId;
 
-        res.send(
+        send(res).send(
             await getAssignedStudents(getPlatform(req), lineItemId, resourceLinkId)
         );
     });
 
-    app.post(PUT_STUDENT_GRADE_VIEW, requestLogger, async (req: any, res: any) => {
+    app.post(PUT_STUDENT_GRADE_VIEW, requestLogger, async (req: Request, res: Response) => {
         const title = getSession(req).title;
         const score = req.body.params;
-        res.send(await putStudentGradeView(getPlatform(req), score, title));
+        send(res).send(await putStudentGradeView(getPlatform(req), score, title));
     });
 
-    app.post(DEEP_LINK_ASSIGNMENT_ENDPOINT, requestLogger, async (req: any, res: any) => {
+    app.post(DEEP_LINK_ASSIGNMENT_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
 
         const contentItems = req.body.contentItems;
         // eslint-disable-next-line prettier/prettier
-        return res.send(await postDeepLinkAssignment(getPlatform(req), contentItems));
+        return send(res).send(await postDeepLinkAssignment(getPlatform(req), contentItems));
     });
 
-    app.post(PUT_STUDENT_GRADE, requestLogger, async (req: any, res: any) => {
+    app.post(PUT_STUDENT_GRADE, requestLogger, async (req: Request, res: Response) => {
 
         const title = getSession(req).title;
         const score = req.body.params;
-        res.send(await putStudentGrade(getPlatform(req), score, title));
+        send(res).send(await putStudentGrade(getPlatform(req), score, title));
     });
 
-    app.delete(DELETE_LINE_ITEM, requestLogger, async (req: any, res: any) => {
+    app.delete(DELETE_LINE_ITEM, requestLogger, async (req: Request, res: Response) => {
         const lineItemId: any = req.query.lineItemId;
 
-        res.send(await deleteLineItem(getPlatform(req), lineItemId));
+        send(res).send(await deleteLineItem(getPlatform(req), lineItemId));
     });
 
-    app.get(GET_GRADES, requestLogger, async (req: any, res: any) => {
-        res.send(await getGrades(getPlatform(req), req.query.lineItemId));
+    app.get(GET_GRADES, requestLogger, async (req: Request, res: Response) => {
+        send(res).send(await getGrades(getPlatform(req), req.query.lineItemId));
     });
 
-    app.get(GET_JWKS_ENDPOINT, requestLogger, async (req: any, res: any) => {
+    app.get(GET_JWKS_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
         const query: any = req.query;
         const consumerTool: ToolConsumer | undefined = getToolConsumerByName(
             query.name
         );
-        res.send(consumerTool);
+        send(res).send(consumerTool);
     });
 
-    app.get(LTI_SESSION_VALIDATION_ENDPOINT, requestLogger, async (req: any, res: any) => {
+    app.get(LTI_SESSION_VALIDATION_ENDPOINT, requestLogger, async (req: Request, res: Response) => {
         logger.debug(`sessionquery: ${JSON.stringify(req.query)}`);
        
-        res.send({ isValid: validateSession( getPlatform(req)) });
+        send(res).send({ isValid: validateSession( getPlatform(req)) });
     });
 };
 
