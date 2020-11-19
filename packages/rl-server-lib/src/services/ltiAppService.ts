929ad8bad2e5707d22c06f41d8fba0caeeb1a2d2
James Stanley <jstanley@unicon.net>
2020-11-19 16:58:06 -0700

    fix a debugging error

--- packages/rl-server-lib/src/services/ltiAppService.ts
+++ packages/rl-server-lib/src/services/ltiAppService.ts
@@ -200,7 +200,7 @@ const getGrades = async (platform: any, lineItemId: any): Promise<any> => {
             resourceLinkId: false
         }));
         logger.debug(
-            " results[0].results - " + JSON.stringify(results[0].results)
+            " results.results - " + JSON.stringify(results)
         );
 
         const membersCollection = await new NamesAndRoles().getMembers(platform, {
