3dfac2567a0b8199189d60b0508ed3a5916713ab
jstanley <jstanley70@gmail.com>
2020-11-30 15:00:38 -0700

    added additional config variables: SESSION_TABLE_NAME and JWK_CACHE_TOKEN_EXPIRY only required for cached lti services

--- packages/rl-server-lib/src/database/entity/Session.ts
+++ packages/rl-server-lib/src/database/entity/Session.ts
@@ -1,23 +1,25 @@
 import {
-    Config,
-    Decorator,
-    Query,
-    Table,
-  } from "dynamo-types";
-  
-  @Decorator.Table({ name: `Session` })
-  export class Session extends Table {
-      
-    @Decorator.HashPrimaryKey("session_id")
-    public static readonly primaryKey: Query.HashPrimaryKey<Session, string>;
-  
-    @Decorator.Writer()
-    public static readonly writer: Query.Writer<Session>;
-  
-    @Decorator.Attribute({ name: "session_id" })
-    public sessionId: string;
-  
-    @Decorator.Attribute({ name: "session" })
-    public session: string;
+  Config,
+  Decorator,
+  Query,
+  Table,
+} from "dynamo-types";
 
-  }
+import { SESSION_TABLE_NAME } from "@asu-etx/rl-shared";
+
+@Decorator.Table({ name: SESSION_TABLE_NAME })
+export class Session extends Table {
+
+  @Decorator.HashPrimaryKey("session_id")
+  public static readonly primaryKey: Query.HashPrimaryKey<Session, string>;
+
+  @Decorator.Writer()
+  public static readonly writer: Query.Writer<Session>;
+
+  @Decorator.Attribute({ name: "session_id" })
+  public sessionId: string;
+
+  @Decorator.Attribute({ name: "session" })
+  public session: string;
+
+}
